6f0d628d6c10621c5662471eca9da57d3315f2ca
Snubb <60389050+Snubb@users.noreply.github.com>
2020-10-13 11:12:09 +0200

    Gjorde uppgifter

--- src/Ovning.java
+++ src/Ovning.java
@@ -1,5 +1,41 @@
+import javax.swing.*;
+
 public class Ovning {
     public static void main(String[] args) {
-        
+        /* -Uppgift 4.1-
+        int minuter = Integer.parseInt(JOptionPane.showInputDialog("Hur många minuter kommer du ringa den här månaden?"));
+        if (minuter < 33) {
+            System.out.println("Du bör välja Kontant.");
+        } else if (minuter > 66) {
+            System.out.println("Du bör välja Plus.");
+        } else {
+            System.out.println("Du bör välja Normal.");
+        }
+         */
+
+        /* -Uppgift 4.2-
+        double radie = Double.parseDouble(JOptionPane.showInputDialog("Vad är cirkelns area"));
+        double omkrets = radie * 2 * 3.1415926536;
+        double area = 3.1415926536 * radie * radie;
+        if (radie > 0) {
+            JOptionPane.showMessageDialog(null, "Omkrets: " + omkrets + "\nArea: " + area);
+        } else {
+            JOptionPane.showMessageDialog(null, "Radien måste vara större än 0");
+        }
+         */
+
+        // -Uppgift 4.3-
+        /*
+        String postnummer = JOptionPane.showInputDialog("Vad är ditt postnummer");
+        int distrikt = Integer.parseInt(postnummer.substring(0,2));
+        if (20 <= distrikt && 62 >= distrikt || distrikt == 65 || distrikt == 66) {
+            System.out.println("Du bor i Götaland");
+        } else if (distrikt >= 80) {
+            System.out.println("Du bor i Norrland");
+        } else {
+            System.out.println("Du bor i Svealand");
+         */
+
+        // -Uppgift
+        }
     }
-}
